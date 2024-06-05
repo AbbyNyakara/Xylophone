25b532ada048fb6f707ff606b13b3695d45a2122
Abigael Nyakara <mogusuab@msu.edu>
2024-06-05 12:28:16 -0400

    add code

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func playSoundButton(_ sender: UIButton) {
+        // Play sound here:
+        
+        // The sound file
+        guard let soundFile = Bundle.main.url(
+         forResource: "Sounds/C", withExtension: "wav"
+        ) else {
+            return
+        }
+        
+        // play the sounds:
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
 }
 
